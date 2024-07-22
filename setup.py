ba46127354d3a381c02bcf8a01ab5ca0d06c1697
Nishantth1 <nishantthakre9@gmail.com>
2024-07-23 00:59:15 +0530

    New Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rabbitmq_auto_scaler',
-    version='0.1.0',
+    version='0.1.2',
     description='A library for auto-scaling RabbitMQ consumers',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
